b5f909e3557260ac5decfa8348a39ebc54b765a3
dev <dev@dev.com>
2024-07-23 18:03:38 +0530

    Override user model

--- users/models.py
+++ users/models.py
@@ -4,8 +4,23 @@ from .managers import CustomUserManager
 
 
 class CustomUser(AbstractUser):
+    ADMIN = 1
+    MANAGER = 2
+    USER = 3
+
+    ROLE_CHOICES = (
+        (ADMIN, 'admin'),
+        (MANAGER, 'manager'),
+        (USER, 'user')
+    )
     username = None
     email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    role = models.PositiveSmallIntegerField(
+        choices=ROLE_CHOICES, blank=True, null=True, default=3)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     objects = CustomUserManager()
