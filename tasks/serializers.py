930ebb1410029c48efeaee897ce34e7e86fdfe18
Arshkhan Pathan <pathanarshkhan@gmail.com>
2024-07-24 22:49:01 +0530

    Refactor code

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -9,11 +9,4 @@ class TasksSerializer(serializers.ModelSerializer):
     created_by = UserLoginSerializer(read_only=True)
     class Meta:
         model = Tasks
-        fields = "__all__"
-
-    def create(self, validated_data):
-        created_by = get_user_model().objects.get(id=1)
-        return Tasks.objects.create(**validated_data, created_by=created_by)
-
-    def update(self, instance, validated_data):
-        return instance.update(**validated_data)
+        fields = ['id', 'name', 'description', 'created_at', 'is_Pending', 'created_by']
--- tasks/views.py
+++ tasks/views.py
@@ -2,43 +2,46 @@ from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
 from tasks.models import Tasks
 from tasks.serializers import TasksSerializer
+from tasks.permissions import IsOwner
 
-
-# Create your views here.
 class TaskCreateListApiView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
-        permission_classes = (IsAuthenticated,)
-        tasks = Tasks.objects.all()
+        tasks = Tasks.objects.filter(created_by=request.user)
         serializer = TasksSerializer(tasks, many=True)
         return Response(serializer.data)
 
     def post(self, request):
         serializer = TasksSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(created_by=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TaskUpdateDeleteApiView(APIView):
+    permission_classes = [IsAuthenticated, IsOwner]
 
     def get(self, request, pk=None):
-        task = Tasks.objects.get(pk=pk)
+        task = get_object_or_404(Tasks, pk=pk, created_by=request.user)
         serializer = TasksSerializer(task)
         return Response(serializer.data)
 
     def put(self, request, pk=None):
-        task = Tasks.objects.get(pk=pk)
+        task = get_object_or_404(Tasks, pk=pk, created_by=request.user)
         serializer = TasksSerializer(task, data=request.data)
+        print("serializer", serializer)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk=None):
-        task = Tasks.objects.get(pk=pk)
+        task = get_object_or_404(Tasks, pk=pk, created_by=request.user)
         task.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
